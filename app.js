4a7d541aa72491a488b9b66458f6124df512d0fe
Nadia Abe <abe.nami0510@gmail.com>
2019-12-02 10:54:42 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const driversRoutes = require('./routes/driversRoutes')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/truckpad-challenge', { useNewUrlParser: true }).then(() => {
+  console.log('connected to mongo database')
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json({ limit: '10mb' }))
+
+app.use(morgan('dev'))
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }),
+)
+
+app.use('/', driversRoutes)
+
+app.listen(5000, () => console.log('server running on PORT 5000!'))
+
+module.exports = app
---
+++ bin/seeds.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Driver = require("../models/Driver");
+
+mongoose
+  .connect("mongodb://localhost/truckpad-challenge", { useNewUrlParser: true })
+  .then(() => console.log("connected to mongo database"));
+
+const drivers = [
+  {
+    name: "teste",
+    state: "São Paulo",
+    city: "São Paulo",
+    address: {
+      state: "São Paulo",
+      country: "Brasil",
+      neighborhood: "Mirandópolis",
+      city: "São Paulo",
+      streetNumber: 1000,
+      streetName: "Avenida Brasil"
+    },
+    documents: [
+      {
+        country: "Brasil",
+        number: 23584851562,
+        docType: "CPF"
+      }
+    ]
+  }
+];
+
+Driver.insertMany(drivers).then(() => {
+  console.log("drivers inserted");
+  mongoose.connection.close();
+});
